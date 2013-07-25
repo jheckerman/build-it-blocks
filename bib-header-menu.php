5fb6459722a09810dec258205c3bdda867949539
Jonathan Heckerman <jheckerman@hotmail.com>
2013-07-25 16:30:17 -0400

    deleted clear:both in bib-header-menu

--- bib-header-menu.php
+++ bib-header-menu.php
@@ -10,7 +10,7 @@
 	</script>
 </head>
 <body>
-	<div style="width:800px; margin-left:auto; margin-right:auto; clear:both">
+	<div style="width:800px; margin-left:auto; margin-right:auto">
 		  <ul style="margin:0px;  list-style-type:none; float:left; height:80px; width:800px; padding:0px; text-align:center; clear:both">
 			  <li style="float:left; width:250px">
 				  <a href="http://build-it-yourself.com"><img src="images/biy-logo.gif"></a>
