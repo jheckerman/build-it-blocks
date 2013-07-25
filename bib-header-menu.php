2a0bd44ac0016ea605f1554630f388d0a49e2ab7
Jonathan Heckerman <jheckerman@hotmail.com>
2013-07-25 16:24:11 -0400

    Revert "Revert "Revert "navigation bar fixes (table/td's ->ul/li's)"""
    This reverts commit 814d6ac77ba37dabc74b3c261f24851e69a77baa.

--- bib-header-menu.php
+++ bib-header-menu.php
@@ -10,52 +10,55 @@
 	</script>
 </head>
 <body>
-	<div style="width:800px; margin-left:auto; margin-right:auto; clear:both">
-		  <ul style="margin:0px;  list-style-type:none; float:left; height:80px; width:800px; padding:0px; text-align:center; clear:both">
-			  <li style="float:left; width:250px">
-				  <a href="http://build-it-yourself.com"><img src="images/biy-logo.gif"></a>
-			  </li>
-			  <li style="float:left; width:110px">
-				  <a href="module-selection-junk.php">
-					<div id="junk" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
-						Junk
-					</div>
-				  </a>
-			  </li>
-			  
-			  <li style="float:left; width:110px">
-				  <a href="module-selection-lego.php">
-					<div id="lego" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
-						LEGO
-					</div>
-				  </a>
-			  </li>
-			  
-			  <li style="float:left; width:110px">
-				  <a href="module-selection-art.php">
-					<div id="art" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
-						Art
-					</div>
-				  </a>
-			  </li>
-			  
-			  <li style="float:left; width:110px">
-				  <a href="module-selection-programming.php">
-					<div id="programming" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
-						Programming
-					</div>
-				  </a>
-			  </li> 
-			  
-			  <li style="float:left; width:110px">
-				  <a href="module-selection-minecraft.php">
-					<div id="minecraft" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
-						Minecraft
-					</div>
-				  </a>
-			  </li> 
-		  </ul>
-	  </div>
-	  <div style="height:1px; width:800px; background-color:#404040; margin-left:auto; margin-right:auto; margin-bottom:-4px; margin-top:-25px; clear:both"> </div>
+	<div align="center">  
+	  
+	  <table align="center" width="800px" height="80px" cellpadding="0" cellspacing="0" >
+		<tr>
+		  <td align="center" width="250">
+			  <a href="http://build-it-yourself.com"><img src="images/biy-logo.gif"></a>
+		  </td>
+		  <td align="center" width="110">
+			  <a href="module-selection-junk.php">
+				<div id="junk" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
+					Junk
+				</div>
+			  </a>
+		  </td>
+		  
+		  <td align="center" width="110">
+			  <a href="module-selection-lego.php">
+				<div id="lego" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
+					LEGO
+				</div>
+			  </a>
+		  </td>
+		  
+		  <td align="center" width="110">
+			  <a href="module-selection-art.php">
+				<div id="art" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
+					Art
+				</div>
+			  </a>
+		  </td>
+		  
+		  <td align="center" width="110">
+			  <a href="module-selection-programming.php">
+				<div id="programming" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
+					Programming
+				</div>
+			  </a>
+		  </td> 
+		  
+		  <td align="center" width="110">
+			  <a href="module-selection-minecraft.php">
+				<div id="minecraft" class="menu-button" onMouseOver="changeImage(this,'images/button-down.png')" onMouseOut="changeImage(this,'images/button-up.png')">
+					Minecraft
+				</div>
+			  </a>
+		  </td> 
+		  
+		</tr>
+	  </table>
+	  <div style="height:1px; width:800px; background-color:#404040; margin-bottom:-4px;"> </div>
 </body>
 </html>
