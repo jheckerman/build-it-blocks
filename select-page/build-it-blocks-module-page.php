3607511187aa129b36c37fff2b3049b82eba56a3
Josh <jjr95@cornell.edu>
2013-07-25 16:14:23 -0400

    Delay on Prev/Next Step

--- select-page/build-it-blocks-module-page.php
+++ select-page/build-it-blocks-module-page.php
@@ -20,6 +20,8 @@
 		var titleArr = new Array(); //contains all application titles
 		var descriptionArr = new Array (); ////contains all application descriptions
 		var stepArr = new Array(); //contains all instruction text
+		var animating=false; //boolean to check if animations are true
+		var delay_speed=900; //speed of the delay for text; used in gotoNextStep() and gotoPrevStep()
 		
 
 		<?php
@@ -60,23 +62,36 @@
 		}
 		
 		function gotoNextStep(){
-			step++;
-			if(step>maxStep) step=0;
-			var newStep = stepArr[step];
-			step++;		//step is used as an index for an array. When step = 0, Step 1 should be showing.
-			document.getElementById("caption2").innerHTML="<h4> Step: " + step + "</h4>"  + newStep;
-			step--;
+			if (!animating){
+				animating=true;
+				setTimeout( function() {
+					step++;
+					if(step>maxStep) step=0;
+					var newStep = stepArr[step];
+					step++;		//step is used as an index for an array. When step = 0, Step 1 should be showing.
+					document.getElementById("caption2").innerHTML="<h4> Step: " + step + "</h4>"  + newStep;
+					step--;
+					animating=false;
+				},delay_speed);
+			}
 		}
 		
 		function gotoPrevStep(){
-			step--;
-			if(step<0) step=maxStep;
-			var newStep = stepArr[step];
-			step++;		//step is used as an index for an array. When step = 0, Step 1 should be showing.
-			document.getElementById("caption2").innerHTML="<h4> Step: " + step + "</h4>"  + newStep;
-			step--;
+		
+			if (!animating){
+				animating=true;
+				setTimeout( function() {
+					step--;
+					if(step<0) step=maxStep;
+					var newStep = stepArr[step];
+					step--;		//step is used as an index for an array. When step = 0, Step 1 should be showing.
+					document.getElementById("caption2").innerHTML="<h4> Step: " + step + "</h4>"  + newStep;
+					step++;
+					animating=false;
+				},delay_speed);
+			}
 		}
-			
+													 
 			 
 		function HideInstr(){
 		$(document).ready(function() {
@@ -291,6 +306,7 @@
 		
   
   </div>
+  
 
   <!-- SlidesJS Required: Link to jQuery -->
   <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
@@ -336,3 +352,4 @@
   <div style="clear:both"><?php include("../biy-bottom-info.html"); ?></div
 </body>
 </html>
+
