368f1fd03406e047763a81f60f494e3a66f3cb2d
Jonathan Heckerman <jheckerman@hotmail.com>
2013-07-26 15:56:46 -0400

    NEW MENU
    awsomeness.

--- bib-main-page.php
+++ bib-main-page.php
@@ -24,147 +24,149 @@
 		}
 	</style>
 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-	<script>
-		$(document).ready(function() {
-			document.getElementById('junki').style.display ='none';
-			document.getElementById('legoi').style.display ='none';
-			document.getElementById('arti').style.display ='none';
-			document.getElementById('programmingi').style.display ='none';
-			document.getElementById('minecrafti').style.display ='none';
-			$('#junk').mouseenter(
-				function() {
-					$('#junkc').stop().stop().stop().animate({'top' : '50px'}, 'fast');				
-					$('#junkc').slideUp();
-					$('#junki').fadeIn();
-					document.getElementById('junki').style.display='block';
-				}
-			);
-			$('#junk').mouseout(
-				function(){
-					document.getElementById('junki').style.display='block';
-					$('#junki').stop().stop().stop().fadeOut();
-					$('#junkc').slideDown();
-					$('#junkc').animate({	'top'	: '0px' , 'height' : '100px'},'fast');
-				}
-			);
-			$('#lego').mouseenter(
-				function () {
-					$('#legoc').stop().stop().stop().animate({'top' : '50px'}, 'fast');	
-					$('#legoc').slideUp();
-					$('#legoi').fadeIn();
-					document.getElementById('legoi').style.display='block';
-				}
-			);
-			$('#lego').mouseout(
-				function(){
-					document.getElementById('legoi').style.display='block';
-					$('#legoi').stop().stop().stop().fadeOut();	
-					$('#legoc').slideDown();
-					$('#legoc').animate({	'top'	: '0px' , 'height' : '100px'},'fast');
-				}
-			);
-			$('#art').mouseenter(
-				function () {
-					$('#artc').stop().stop().stop().animate({'top' : '50px'}, 'fast');		
-					$('#artc').slideUp();
-					$('#arti').fadeIn();
-					document.getElementById('arti').style.display='block';
-				}
-			);
-			$('#art').mouseout(
-				function(){
-					document.getElementById('arti').style.display='block';
-					$('#arti').stop().stop().stop().fadeOut();
-					$('#artc').slideDown();
-					$('#artc').animate({	'top'	: '0px' , 'height' : '100px'},'fast');
-				}
-			);
-			$('#programming').mouseenter(
-				function () {
-					$('#programmingc').stop().stop().stop().animate({'top' : '50px'}, 'fast');			
-					$('#programmingc').slideUp();
-					$('#programmingi').fadeIn();
-					document.getElementById('programmingi').style.display='block';
-				}
-			);
-			$('#programming').mouseout(
-				function(){
-					document.getElementById('programmingi').style.display='block';
-					$('#programmingi').stop().stop().stop().fadeOut();
-					$('#programmingc').slideDown();
-					$('#programmingc').animate({	'top'	: '0px' , 'height' : '100px'},'fast');
-				}
-			);
-			
-			
-			$('#minecraft').mouseenter(
-				function () {
-					$('#minecraftc').stop().stop().stop().animate({'top' : '50px'}, 'fast');			
-					$('#minecraftc').slideUp();
-					$('#minecrafti').fadeIn();
-					document.getElementById('minecrafti').style.display='block';
-				}
-			);
-			$('#minecraft').mouseout(
-				function(){
-					document.getElementById('minecrafti').style.display='block';
-					$('#minecrafti').stop().stop().stop().fadeOut();
-					$('#minecraftc').slideDown();
-					$('#minecraftc').animate({	'top'	: '0px' , 'height' : '100px'},'fast');
-				}
-			);
-			
-			
-		});
-	</script>
+	
+	
+	<!--NEW ON 7/26/13-->
+	<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen"/>
+        <style>
+			span.reference{
+				position:fixed;
+				left:10px;
+				bottom:10px;
+				font-size:12px;
+			}
+			span.reference a{
+				color:#aaa;
+				text-transform:uppercase;
+				text-decoration:none;
+				text-shadow:1px 1px 1px #000;
+				margin-right:30px;
+			}
+			span.reference a:hover{
+				color:#ddd;
+			}
+			ul.sdt_menu{
+				margin-top:150px;
+			}
+		</style>
+	
 </head>
 
 <body>
 	<?php include("biy-header.html");?>
-	<table style="margin:auto; height:400px;" border="0px">
-		<tr>
-			<td onClick="document.location.href='select-page/module-selection-junk.php';" id="junk" style="width:200px; opacity:1; cursor:pointer;cursor:hand">
-				<img id="junki" class="icon" src="menu-icons/menu-junk.jpg">
-				<div id="junkc" class="type-select">
-				<p>
-					<br>Junk</p>
-				</div>
-			</td>
-			<td onClick="document.location.href='select-page/module-selection-lego.php';" id="lego" style="width:200px; opacity:1; cursor:pointer;cursor:hand">
-			<img id="legoi" class="icon" src="menu-icons/menu-lego.jpg">
-				<div id="legoc" class="type-select">
-					<p>
-						<br>LEGO
-					</p>
-				</div>
-			</td>
-			<td onClick="document.location.href='select-page/module-selection-art.php';" id="art" style="width:200px; opacity:1; cursor:pointer;cursor:hand">
-				<img id="arti" class="icon" src="menu-icons/menu-art.jpg">
-				<div id="artc" class="type-select">
-					<p>
-						<br>Art
-					</p>
-				</div>
-			</td>
-			<td onClick="document.location.href='select-page/module-selection-programming.php';" id="programming" style="width:200px; opacity:1; cursor:pointer;cursor:hand">
-				<img id="programmingi" class="icon" src="menu-icons/menu-pragramming.jpg">
-				<div id="programmingc" class="type-select">
-					<p>
-						<br>Programming
-					</p>
-				</div>
-			</td>
-			
-			<td onClick="document.location.href='select-page/module-selection-minecraft.php';" id="minecraft" style="width:200px; opacity:1; cursor:pointer;cursor:hand">
-				<img id="minecrafti" class="icon" src="menu-icons/menu-minecraft.png" style="height:180px; width:180px;">
-				<div id="minecraftc" class="type-select">
-					<p>
-						<br>Minecraft
-					</p>
-				</div>
-			</td>
-			
-		</tr>
-	</table>
+	
+	
+			<ul id="sdt_menu" class="sdt_menu">
+				<li>
+					<a href="select-page/module-selection-junk.php">
+						<img src="menu-icons/menu-junk.jpg" alt=""/>
+						<span class="sdt_active"></span>
+						<span class="sdt_wrap">
+							<span class="sdt_link"><div style="text-align:center">Junk</div></span>
+						</span>
+					</a>
+				</li>
+				<li>
+					<a href="select-page/module-selection-lego.php">
+						<img src="menu-icons/menu-lego.jpg" alt=""/>
+						<span class="sdt_active"></span>
+						<span class="sdt_wrap">
+							<span class="sdt_link"><div style="text-align:center">Lego</div></span>
+						</span>
+					</a>
+				</li>
+				<li>
+					<a href="select-page/module-selection-art.php">
+						<img src="menu-icons/menu-art.jpg" alt=""/>
+						<span class="sdt_active"></span>
+						<span class="sdt_wrap">
+							<span class="sdt_link"><div style="text-align:center">Art</div></span>
+						</span>
+					</a>
+				</li>
+				<li>
+					<a href="select-page/module-selection-programming.php">
+						<img src="menu-icons/menu-programming.jpg" alt=""/>
+						<span class="sdt_active"></span>
+						<span class="sdt_wrap">
+							<span class="sdt_link"><div style="text-align:center">Programming</div></span>
+						</span>
+					</a>
+				</li>
+				<li>
+					<a href="select-page/module-selection-minecraft.php">
+						<img src="menu-icons/menu-minecraft.jpg" alt=""/>
+						<span class="sdt_active"></span>
+						<span class="sdt_wrap">
+							<span class="sdt_link"><div style="text-align:center">Minecraft</div></span>
+						</span>
+					</a>
+				</li>
+			</ul>
+	
+	
+	
+	<!-- The JavaScript -->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript" src="jquery.easing.1.3.js"></script>
+        <script type="text/javascript">
+            $(function() {
+				/**
+				* for each menu element, on mouseenter, 
+				* we enlarge the image, and show both sdt_active span and 
+				* sdt_wrap span. If the element has a sub menu (sdt_box),
+				* then we slide it - if the element is the last one in the menu
+				* we slide it to the left, otherwise to the right
+				*/
+                $('#sdt_menu > li').bind('mouseenter',function(){
+					var $elem = $(this);
+					$elem.find('img')
+						 .stop(true)
+						 .animate({
+							'width':'210px',
+							'height':'210px',
+							'left':'0px'
+						 },400,'easeOutBack')
+						 .andSelf()
+						 .find('.sdt_wrap')
+					     .stop(true)
+						 .animate({'top':'110px'},500,'easeOutBack')
+						 .andSelf()
+						 .find('.sdt_active')
+					     .stop(true)
+						 .animate({'height':'83px'},300,function(){
+						var $sub_menu = $elem.find('.sdt_box');
+						if($sub_menu.length){
+							var left = '210px';
+							if($elem.parent().children().length == $elem.index()+1)
+								left = '-210px';
+							$sub_menu.show().animate({'left':left},200);
+						}	
+					});
+				}).bind('mouseleave',function(){
+					var $elem = $(this);
+					var $sub_menu = $elem.find('.sdt_box');
+					if($sub_menu.length)
+						$sub_menu.hide().css('left','0px');
+					
+					$elem.find('.sdt_active')
+						 .stop(true)
+						 .animate({'height':'0px'},300)
+						 .andSelf().find('img')
+						 .stop(true)
+						 .animate({
+							'width':'0px',
+							'height':'0px',
+							'left':'85px'},400)
+						 .andSelf()
+						 .find('.sdt_wrap')
+						 .stop(true)
+						 .animate({'top':'25px'},500);
+				});
+            });
+        </script>
+	
+	
 </body>
 </html>
