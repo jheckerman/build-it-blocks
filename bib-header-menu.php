61ca6ce6e2d8c22551b78116f0cc0013b5e91b4d
Valeria Staneva <valeria.staneva@gmail.com>
2013-08-05 17:24:08 -0400

    ALL STYLES FOR HEADER FILE ARE IN STYLE SHEET
    and I'm feeling good :)))

--- bib-header-menu.php
+++ bib-header-menu.php
@@ -11,18 +11,18 @@
 <body>
 	<!-- the 'clear:both' modifier centers the div but it looks like it messes up with other stuff. If there's a possibility, replace it with something else. -->
 	<!-- Still, centering is our first priority. Too sadly, 'align:center' is deprecated. -->
-	<div style="width:800px; margin-left:auto; margin-right:auto; clear:both;">
-		<ul style="margins:0px;  list-style-type:none; float:left; height:50px; width:800px; padding:0px; text-align:center;">
+	<div id="header-menu-container" >
+		<ul id="navigation-bar">
 		<!-- for some unknown reason the next two lines were added automatically by Github sync tool, and even though the div and the ul are not closed anywhere, -->
 		<!-- it does HTML magic and doesn't want to center without them. -->
 		<!-- I guess HTML is tough for no reason from time to time. And life is so. -->
-			<div style="width:800px; margin-left:auto; margin-right:auto">
-				<ul style="margin:0px;  list-style-type:none; float:left; height:50px; width:800px; padding:0px; text-align:center; clear:both"> <!--list-type-style:none get rid of the bullets in the list -->
-					<li style="float:left; height:35px"> <!-- I'm not really sure 35px is the right size, we need to do some calculation to make the BIY logo align with the buttons. -->
+			<div style="width:800px; margin-left:auto; margin-right:auto"> <!--HTML MAGIC LINE 1-->
+				<ul style="margin:0px;  list-style-type:none; float:left; height:50px; width:800px; padding:0px; text-align:center; clear:both"> <!--HTML MAGIC LINE 2-->
+					<li id="biy-logo"> <!-- I'm not really sure 35px is the right size, we need to do some calculation to make the BIY logo align with the buttons. -->
 					<!-- so, the leftest item in the navigation bar is the BIY logo that is an active link to the main BIY website-->
-						<a href="http://build-it-yourself.com"><img src="images/biy-logo.gif" style="max-height:100%"></a>
+						<a href="http://build-it-yourself.com"><img src="images/biy-logo.gif"></a>
 					</li>			  
-					<li style="float:left;">
+					<li class="nav-button">
 					<!-- the list items themselves float to the left, but the insides of divs are centered -->
 						<a href="../bib-main-page.php">
 						<!-- inside of the reference we put a div that contains the button. Unless clicked, it is on button-up.png by default, and goes to button-down.png when we hover over it. -->
@@ -32,7 +32,7 @@
 							</div>
 						</a>
 					</li>
-					<li style="float:left;">
+					<li class="nav-button">
 					<!-- the list items themselves float to the left, but the insides of divs are centered -->
 						<a href="module-selection-junk.php">
 						<!-- inside of the reference we put a div that contains the button. Unless clicked, it is on button-up.png by default, and goes to button-down.png when we hover over it. -->
@@ -42,7 +42,7 @@
 							</div>
 						</a>
 					</li>
-					<li style="float:left;">
+					<li class="nav-button">
 					<!-- the list items themselves float to the left, but the insides of divs are centered -->
 						<a href="module-selection-lego.php">
 						<!-- inside of the reference we put a div that contains the button. Unless clicked, it is on button-up.png by default, and goes to button-down.png when we hover over it. -->
@@ -52,7 +52,7 @@
 							</div>
 						</a>
 					</li>
-					<li style="float:left;">
+					<li class="nav-button">
 					<!-- the list items themselves float to the left, but the insides of divs are centered -->
 						<a href="module-selection-art.php">
 						<!-- inside of the reference we put a div that contains the button. Unless clicked, it is on button-up.png by default, and goes to button-down.png when we hover over it. -->
@@ -62,7 +62,7 @@
 							</div>
 						</a>
 					</li>
-					<li style="float:left;">
+					<li class="nav-button">
 					<!-- the list items themselves float to the left, but the insides of divs are centered -->
 						<a href="module-selection-programming.php">
 						<!-- inside of the reference we put a div that contains the button. Unless clicked, it is on button-up.png by default, and goes to button-down.png when we hover over it. -->
@@ -72,7 +72,7 @@
 							</div>
 						</a>
 					</li> 
-					<li style="float:left;">
+					<li class="nav-button">
 					<!-- the list items themselves float to the left, but the insides of divs are centered -->
 						<a href="module-selection-minecraft.php">
 						<!-- inside of the reference we put a div that contains the button. Unless clicked, it is on button-up.png by default, and goes to button-down.png when we hover over it. -->
@@ -84,10 +84,10 @@
 					</li>
 				</ul>
 			</div> <!-- see, we do not close the div and the ul and I hate it, but it works when it's written wrong. It, on the other hand, refuses to work when written correctly. Makes me a bit sad, but I almost got over it. -->
-			<div style="height:1px; width:800px; background-color:#404040; margin-left:auto; margin-right:auto; margin-bottom:12px; margin-top:1px; clear:both; position:relative;">
+			<div id="header-line">
 	  	</div>
 		<!-- it turns out that without putting clear:none somewhere, we cannot center without using the clear:both modifier. -->
 		<!-- That's why we put it in an empty span. And from here on, we can center the divs, spans, uls and tables that come next. -->
-	  	<span style="clear:none;"> </span> 
+	  	<span class="empty-span"> </span> 
 </body>
 </html>
